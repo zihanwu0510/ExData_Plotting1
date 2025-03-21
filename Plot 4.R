88624db997e764583e9a802c6e92c637ab3d4d57
zihanwu0510 <zihanwu0313@gmail.com>
2025-03-22 00:46:58 +0900

    Create Plot 4.R

---
+++ Plot 4.R
@@ -0,0 +1,30 @@
+### read data from txt
+data_power <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
+data_power$Date <- as.Date(data_power$Date, format = "%d/%m/%Y")
+
+
+### subset data
+data_power_plot <- subset(data_power, data_power$Date >= as.Date("2007-02-01") & data_power$Date <= as.Date("2007-02-02"))
+
+### Date+Time
+data_power_plot$DateTime <- as.POSIXct(paste(data_power_plot$Date, data_power_plot$Time))
+
+### Plot 4
+par(mfrow = c(2,2))
+with(data_power_plot, {
+  plot(data_power_plot$Global_active_power~data_power_plot$DateTime, type = "l", ylab = "Global Active Power", xlab = "", xaxt = "n")
+  axis.POSIXct(1, at = seq(as.POSIXct("2007-02-01"), as.POSIXct("2007-02-03"), by = "day"), 
+               format = "%a")
+  plot(data_power_plot$Voltage~data_power_plot$DateTime, type = "l", ylab = "Voltage", xlab = "dat3time", xaxt = "n")
+  axis.POSIXct(1, at = seq(as.POSIXct("2007-02-01"), as.POSIXct("2007-02-03"), by = "day"), 
+               format = "%a")
+  plot(data_power_plot$Sub_metering_1~data_power_plot$DateTime, type = "l", col = "black", ylab = "Energy sub metering", xlab = "", xaxt = "n")
+  lines(data_power_plot$Sub_metering_2~data_power_plot$DateTime, col="red")
+  lines(data_power_plot$Sub_metering_3~data_power_plot$DateTime, col="blue")
+  legend("topright", legend = c ("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c ("black", "red", "blue"), cex = 0.5, lty = 1, lwd = 1, bty = "n")
+  axis.POSIXct(1, at = seq(as.POSIXct("2007-02-01"), as.POSIXct("2007-02-03"), by = "day"), 
+               format = "%a")
+  plot(data_power_plot$Global_reactive_power~data_power_plot$DateTime, type = "l", ylab = "Global_reactive_power", xlab = "datetime", xaxt = "n")
+  axis.POSIXct(1, at = seq(as.POSIXct("2007-02-01"), as.POSIXct("2007-02-03"), by = "day"), 
+               format = "%a")
+})
